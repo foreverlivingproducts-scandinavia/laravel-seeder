14c777a9ee38488161e25596c25643e8a28528ca
Joakim Johansson <joakim.johansson@foreverliving.se>
2018-10-29 22:56:36 +0100

    renaming

--- scripts/code-coverage.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-vendor/bin/phpunit \
-    --colors=never \
-    --coverage-text \
-    --coverage-html coverage
--- scripts/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-set -e
-
-composer install
-
-exec "$@"
